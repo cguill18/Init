9d0b9964361463553825b0d51d9cb1f49b3808fe
clementine <clementineguillot@gmail.com>
2021-03-07 17:12:28 +0100

    Remise en forme du niveau 1
    le niveau 1 a maintenant un textfield opé, la fonction qui ouvre une fenetre popup quand on click sur le mot "avancer" n'est pas operationnelle

--- Init/app/src/main/java/com/example/init/Niveau1.kt
+++ Init/app/src/main/java/com/example/init/Niveau1.kt
@@ -1,11 +1,26 @@
 package com.example.init
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.PopupWindow
+import androidx.appcompat.app.AlertDialog
 
 class Niveau1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_niveau1)
     }
+
+    fun openDescription(view: View){
+        /*val mypopup = AlertDialog.Builder(this)
+        mypopup.setTitle("Description de Avancer")
+        mypopup.setMessage("vous devez faire une fonction qui fait avancer votre personnage du nombre de pas indiqué")
+        mypopup.setPositiveButton("ok",DialogInterface.OnClickListener {dialog, id -> })
+        mypopup.setNegativeButton("non",DialogInterface.OnClickListener {dialog, id -> })
+        mypopup.show()*/
+        //cette fonction marche pas, elle doit normalement ouvrir une fenetre popup 
+    }
+
 }
