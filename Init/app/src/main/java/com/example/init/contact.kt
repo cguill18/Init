20b5087563935db35b18984f58e03eee6a4c75e5
Amandine <amandine.fradet@live.fr>
2021-03-13 19:58:27 +0100

    Contact v2 avec une ActionBar
    Encore à tester. devrait permettre de retourner sur la page précédente (???)

--- Init/app/src/main/java/com/example/init/contact.kt
+++ Init/app/src/main/java/com/example/init/contact.kt
@@ -3,26 +3,24 @@ package com.example.init
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
 
-class contact : AppCompatActivity() {
-
-    //bouton de retour
-    lateinit var retour : Button
-
-    //fonction de base de l'activite
+class Contact : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
 
-        //initialise le bouton
-    retour = findViewById(R.id.retour)
+        val actionBar = supportActionBar
 
-        //retour dans MainActivity après clic sur le bouton
-        retour.setOnClickListener{
-            val intentRetour : Intent = Intent(this, MainActivity::class.java)
-            startActivity(intentRetour)
-        }
+         actionBar!!.title = "Pour nous joindre"
+
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        actionBar.setDisplayHomeAsUpEnabled(true)
     }
-}
 
+    override fun onSupportNavigateUp() : Boolean{
+        onBackPressed()
+        return true
+    }
+
+}
